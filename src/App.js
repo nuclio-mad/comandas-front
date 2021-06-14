b88b0d7e87e00353d12abdf76e551648fc94e175
Daniel Herrero <danielherrerohernando@gmail.com>
2021-06-14 21:01:33 +0200

    feat: adding select and api calls

--- src/App.js
+++ src/App.js
@@ -5,26 +5,37 @@ import axios from 'axios';
 import './App.css';
 
 
-const ComandaCard = ({name, camarero, cliente}) => {
+const ComandaCard = ({name, camarero, cliente, onDelete}) => {
   return <div className="card">
     <h5>{name}</h5>
-    <p>Camarero: {camarero.name}</p>
-    <p>Cliente: {cliente.name}</p>
+    <p>Camarero: {camarero?.name || 'Unknown'}</p>
+    <p>Cliente: {(cliente && cliente.name) || 'Unknown'}</p>
+    <button onClick={onDelete}>Delete</button>
   </div>
 }
 
 const CreateComanda = ({addComanda}) => {
   const [text, setText] = React.useState('');
+  const [selectedCamarero, setSelectedCamarero] = React.useState('');
 
-  const handleSubmit = async () => {
-    console.log('primer paso handleSubmit')
-    const data = await addComanda(text);
-    console.log('soy el data resuelto de la promesa', data)
-    console.log('tercer paso handleSubmit')
-    setText('')
+  const [camareros, setCamareros] = React.useState([])
+
+  React.useEffect(() => {
+    const getCamareros = async () => {
+      const response = await axios.get('http://localhost:3000/camareros')
+      setCamareros(response.data)
+    }
+    getCamareros()
+  },[])
+
+  const handleSubmit = () => {
+    addComanda(text, selectedCamarero);
+    setText('');
+    setSelectedCamarero('')
   }
-  return <div>
+  return <div className="coolDiv">
     <input value={text} onChange={event => setText(event.target.value)}></input>
+    <select value={selectedCamarero} onChange={event => setSelectedCamarero(event.target.value)}>{camareros.map(camarero => <option value={camarero._id}>{camarero.name}</option>)}</select>
     <button onClick={handleSubmit}>Add comanda</button>
   </div>
 }
@@ -37,7 +48,7 @@ const App = () => {
     // const getComandas = async () => {
     //   const response = axios.get('http://localhost:3000/comandas');
     //   console.log('hey soy lo que devuelve el back sin await', response)
-    //   setComandas(response)
+    //   setComandas(response.data)
     // }
     // getComandas()
     axios.get('http://localhost:3000/comandas')
@@ -49,11 +60,11 @@ const App = () => {
     })
   }, [refresh])
 
-  const addComanda = (name) => {
+  const addComanda = (name, camareroId, clientId) => {
     axios.post('http://localhost:3000/comandas', {
-      name: name,
-      camareroId: "60c39fd814bbbb0fab4b3b9e",
-      clientId: "60c368d50f599803e18bb235"
+      name,
+      camareroId,
+      clientId
     })
     .then(function (response) {
       console.log('segundo paso handleSubmit')
@@ -62,13 +73,19 @@ const App = () => {
     .catch(function (error) {
       console.log(error);
     });
-    return new Promise((resolve, reject) => {
-      console.log('Hola soy la promesa nada más empezar')
-      setTimeout(() => {
-        console.log('Hola soy la promesa a punto de completarse')
-        resolve('Hola soy el valor retornado de la promesa')
-      }, 5000)
-    })
+  }
+
+  const deleteComanda = id => {
+    // va el código que llama al endpoint de borrar
+    axios.delete('http://localhost:3000/comandas', {
+      data:{
+          'id': id
+        }
+      })
+      .then(() => {
+        toggle(!refresh)
+      })
+      .catch(console.log)
   }
 
 
@@ -81,6 +98,7 @@ const App = () => {
         name={comanda.name}
         camarero={comanda.camarero}
         cliente={comanda.cliente}
+        onDelete={() => deleteComanda(comanda._id)}
         />)}
       </ul>
       <CreateComanda addComanda={addComanda}></CreateComanda>
